054dac3d9705d0bf974b551e4b81a50582d10086
nopara73 <adam.ficsor73@gmail.com>
2018-02-22 19:19:11 +0100

    Initial commit

--- NBitcoinP2pOsxBug/Program.cs
+++ NBitcoinP2pOsxBug/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using NBitcoin;
+using NBitcoin.Protocol;
+using NBitcoin.Protocol.Behaviors;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace NBitcoinP2pOsxBug
 {
@@ -6,7 +11,62 @@ namespace NBitcoinP2pOsxBug
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connectionParameters = new NodeConnectionParameters();
+
+            var addressManagerFilePath = "AddressManager.dat";
+            AddressManager addressManager;
+            try
+            {
+                addressManager = AddressManager.LoadPeerFile(addressManagerFilePath);
+                Console.WriteLine($"Loaded {nameof(AddressManager)} from `{addressManagerFilePath}`.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"{nameof(AddressManager)} did not exist at `{addressManagerFilePath}`. Initializing new one.");
+                addressManager = new AddressManager();
+            }
+
+            connectionParameters.TemplateBehaviors.Add(new AddressManagerBehavior(addressManager));
+
+            var nodes = new NodesGroup(Network.Main, connectionParameters,
+                new NodeRequirement
+                {
+                    RequiredServices = NodeServices.Network,
+                    MinVersion = ProtocolVersion.SENDHEADERS_VERSION
+                })
+            {
+                NodeConnectionParameters = connectionParameters
+            };
+
+            Console.WriteLine("Start connecting to nodes...");
+            nodes.Connect();
+
+            try
+            {
+                int i = 0;
+                while (i < 42) // 7 minutes
+                {
+                    Console.WriteLine($"Witing for a connection for {i / (double)6} minutes....");
+                    if (nodes.ConnectedNodes.Count >= 1)
+                    {
+                        Console.WriteLine("SUCCESSFULLY FOUND A CONNECTION");
+                        Console.WriteLine("Press a key to exit...");
+                        Console.ReadKey();
+                        return;
+                    }
+                    Task.Delay(TimeSpan.FromSeconds(10)).GetAwaiter().GetResult();
+                    i++;
+                }
+                Console.WriteLine("DID NOT FIND A CONNECTION");
+                Console.WriteLine("Press a key to exit...");
+                Console.ReadKey();
+            }
+            finally
+            {
+                Console.WriteLine($"Saving {nameof(AddressManager)} to `{addressManagerFilePath}`.");
+                addressManager.SavePeerFile(addressManagerFilePath, Network.Main);
+                nodes.Dispose();
+            }
         }
     }
 }
